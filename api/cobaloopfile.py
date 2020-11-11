bca52fe5b4f7676cc00ab2a3ad7fc165ee2e1a75
fakhrinail <fakhrinailwbw@gmail.com>
2020-11-11 20:33:18 +0700

    Loop file dalam folder
    Udah pake relative path harusnya bisa di masing2

--- api/cobaloopfile.py
+++ api/cobaloopfile.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import glob
 import os
 
-path = Path(__file__).parent / "./testrelativepath/"
+path = Path(__file__).parent / "./uploaded_files/"
 for filename in glob.glob(os.path.join(path, '*.html')):
     with open(os.path.join(os.getcwd(), filename), 'r') as f:
         namafileasli = os.path.basename(filename)
