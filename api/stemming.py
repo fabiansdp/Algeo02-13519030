62c0afc5460f0ca3ba25ce60de3c3af1a2f429f4
ferdyirawanf <ferdyirawanf@gmail.com>
2020-11-12 00:43:14 +0700

    udah dinamis input filenya

--- api/stemming.py
+++ api/stemming.py
@@ -1,7 +1,8 @@
-import os, nltk, os.path, re, string, sys, getopt, Sastrawi
+import os, nltk, os.path, re, string, sys, getopt, Sastrawi, glob2
 
 from nltk.stem.porter import PorterStemmer
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from pathlib import Path
 
 def hanya_huruf( input ):
    r = re.match('^[a-zA-Z]+$', input)
@@ -11,56 +12,19 @@ def hanya_huruf( input ):
       return True
 
 def stemming_file():
-    list_input = ['file1.txt', 
-                    'file2.txt',
-                    'file3.txt',
-                    'file4.txt',
-                    'file5.txt',
-                    'file6.txt',
-                    'file7.txt',
-                    'file8.txt',
-                    'file9.txt',
-                    'file10.txt',
-                    'file11.txt',
-                    'file12.txt',
-                    'file13.txt',
-                    'file14.txt',
-                    'file15.txt']
+    global list_output
 
-    hasil_file1 = []
-    hasil_file2 = []
-    hasil_file3 = []
-    hasil_file4 = []
-    hasil_file5 = []
-    hasil_file6 = []
-    hasil_file7 = []
-    hasil_file8 = []
-    hasil_file9 = []
-    hasil_file10 = []
-    hasil_file11 = []
-    hasil_file12 = []
-    hasil_file13 = []
-    hasil_file14 = []
-    hasil_file15 = []
+    path = Path(__file__).parent / './uploaded_files/'
+    list_dir = [name for name in glob2.glob(os.path.join(path, '*.txt'))]
 
-    list_output = [hasil_file1,
-                    hasil_file2,
-                    hasil_file3,
-                    hasil_file4,
-                    hasil_file5,
-                    hasil_file6,
-                    hasil_file7,
-                    hasil_file8,
-                    hasil_file9,
-                    hasil_file10,
-                    hasil_file11,
-                    hasil_file12,
-                    hasil_file13,
-                    hasil_file14,
-                    hasil_file15]
+    list_input = []
+    for i in range (len(list_dir)):
+        list_input.append(os.path.basename(list_dir[i]))
 
-    for x in range(15):
-        f = open(list_input[x],"r")
+    list_output = [[0 for x in range(0)] for x in range(len(list_input))]
+
+    for x in range(len(list_dir)):
+        f = open(os.path.join(os.getcwd(), list_dir[x]),"r")
         fcontent = f.read()
         lines = fcontent.split()
         f.close()
@@ -76,29 +40,14 @@ def stemming_file():
                     list_output[x].append(word)
             else:
                 pass
-    
-    stemming_file.list_output = list_output
-    stemming_file.hasil_file1 = hasil_file1
-    stemming_file.hasil_file2 = hasil_file2
-    stemming_file.hasil_file3 = hasil_file3
-    stemming_file.hasil_file4 = hasil_file4
-    stemming_file.hasil_file5 = hasil_file5
-    stemming_file.hasil_file6 = hasil_file6
-    stemming_file.hasil_file7 = hasil_file7
-    stemming_file.hasil_file8 = hasil_file8
-    stemming_file.hasil_file9 = hasil_file9
-    stemming_file.hasil_file10 = hasil_file10
-    stemming_file.hasil_file11 = hasil_file11
-    stemming_file.hasil_file12 = hasil_file12
-    stemming_file.hasil_file13 = hasil_file13
-    stemming_file.hasil_file14 = hasil_file14
-    stemming_file.hasil_file15 = hasil_file15
 
 def stemming_query():
+    global hasil_query
+
     factory = StemmerFactory()
     stemmer = factory.create_stemmer()
 
-    query = '....'
+    query = '...'
     lines = query.split()
     hasil_query = []
     for word in lines:
@@ -106,5 +55,3 @@ def stemming_query():
         word = stemmer.stem(word)
         if (word not in nltk.corpus.stopwords.words('indonesian')):
             hasil_query.append(word)
-    
-    stemming_query.hasil_query = hasil_query
